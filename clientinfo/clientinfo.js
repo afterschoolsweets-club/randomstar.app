680752e004688ad3c300c77908b21dde1aa69ac8
Naruka <themunyang21@naver.com>
2021-11-18 20:17:13 +0900

    재수정

--- clientinfo/clientinfo.js
+++ clientinfo/clientinfo.js
@@ -290,7 +290,9 @@ var clientInfo = {
      document.getElementById("location_info").appendChild(phylocation_maplink);
      document.getElementById("service_provider").innerHTML = providerinfo;
      if(navigator.userAgentData){ // UA Data: 크로미엄(Chromium) 기반 브라우저에서 시범 지원 (90 버전 이상)
-      var browserType = `${navigator.userAgentData.brands[2].brand} (Version ${navigator.userAgentData.brands[2].version})`;
+      var browserBrand = (navigator.userAgentData.brands[0].toLowerCase().indexOf("not a brand") < 0) ? navigator.userAgentData.brands[0].brand : navigator.userAgentData.brands[2].brand;
+      var browserVersion = (navigator.userAgentData.brands[0].toLowerCase().indexOf("not a brand") < 0) ? navigator.userAgentData.brands[0].version : navigator.userAgentData.brands[2].version;
+      var browserType = `${browserBrand} (Version ${browserVersion})`;
       var isMobile = (navigator.userAgentData.mobile == true) ? "Mobile" : "Desktop";
       document.getElementById("client_ua").innerHTML = `${isMobile} ${browserType}`;
      }else if(navigator.userAgent){document.getElementById("client_ua").innerHTML = navigator.userAgent;}
