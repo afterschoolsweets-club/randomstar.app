0a4a9970f671716493dc6eee0451242137fd6e2a
Rina Hayasaka <themunyang21@naver.com>
2023-08-12 20:20:52 +0900

    변환표 추가 등

--- calc-bytes/calc_bytes.js
+++ calc-bytes/calc_bytes.js
@@ -24,7 +24,7 @@
 
     var decBytes = new Number(rawDecBytes * decUnit);
 
-    document.getElementById("bytes_binary").value = decBytes / binUnit;
+    document.getElementById("bytes_binary").value = (decBytes / binUnit).toFixed(16);
    }else if(document.getElementById("bin2dec").checked == true){
     document.getElementById("bytes_decimal").disabled = true;
     document.getElementById("bytes_binary").disabled = false;
@@ -34,10 +34,53 @@
 
     var binBytes = new Number(rawBinBytes * binUnit);
 
-    document.getElementById("bytes_decimal").value = binBytes / decUnit;
+    document.getElementById("bytes_decimal").value = (binBytes / decUnit).toFixed(16);
    }
+  },
+  otherResults : function(){
+   document.getElementById("other_results").innerHTML = "";
 
-   
+   var rawDecBytes = document.getElementById("bytes_decimal").value;
+   var rawBinBytes = document.getElementById("bytes_binary").value;
 
+   var rawDecUnit = document.getElementById("decimal_unit").value;
+   var rawBinUnit = document.getElementById("binary_unit").value;
+
+   var unitDelta = new Number(rawDecUnit)-new Number(rawBinUnit);
+
+   if(document.getElementById("dec2bin").checked == true){
+    var resultList = document.createElement("ul");
+    resultList.style.listStyleType = "none";
+    var binaryUnits = document.getElementById("binary_unit").options.length;
+
+    var baseBinUnit = Math.pow(1024,new Number(rawBinUnit));
+    var baseUnitDelta = Math.pow(1024,unitDelta);
+
+    for(b=0;b<binaryUnits;b++){
+     var rawResult = ((baseUnitDelta * (rawDecBytes * baseBinUnit)) / Math.pow(1024,b)).toFixed(16);
+     var resultItem = document.createElement("li");
+     if(b == new Number(rawBinUnit)){resultItem.style.fontWeight = "bold";}
+     resultItem.appendChild(document.createTextNode(`${rawResult} ${unitsToConvertB[b]}`));
+     resultList.appendChild(resultItem);
+    }
+
+   }else if(document.getElementById("bin2dec").checked == true){
+    var resultList = document.createElement("ul");
+    resultList.style.listStyleType = "none";
+    var decimalUnits = document.getElementById("decimal_unit").options.length;
+
+    var baseDecUnit = Math.pow(1000,new Number(rawDecUnit));
+    var baseUnitDelta = Math.pow(1024,unitDelta);
+
+    for(d=0;d<decimalUnits;d++){
+     var rawResult = ((baseUnitDelta * (rawBinBytes * baseDecUnit)) / Math.pow(1000,d)).toFixed(16);
+     var resultItem = document.createElement("li");
+     if(d == new Number(rawDecUnit)){resultItem.style.fontWeight = "bold";}
+     resultItem.appendChild(document.createTextNode(`${rawResult} ${unitsToConvertD[d]}`));
+     resultList.appendChild(resultItem);
+    }
+
+   }
+   document.getElementById("other_results").appendChild(resultList);
   }
  };
